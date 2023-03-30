e5d262aaa66034bd915bd53123fa96dd6b54745e
Mikhail <113590874+the-ugolkov@users.noreply.github.com>
2023-03-30 15:37:45 +0700

    create models Ad AdContent and Response

--- B_Board/ads/models.py
+++ B_Board/ads/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from ads.res import CAT
+
+
+class Ad(models.Model):
+    title = models.CharField(max_length=128)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.CharField(max_length=20, choices=CAT)
+    date_create = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class AdContent(models.Model):
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE, related_name='content')
+    content = models.FileField(upload_to='ad_content/', blank=True)
+
+
+class Response(models.Model):
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_create = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text[:20]+'...'
