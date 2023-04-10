1f61338e5e3ee45a2bb65e2fbb9c92519045732b
Mikhail <113590874+the-ugolkov@users.noreply.github.com>
2023-04-10 22:01:58 +0700

    переопределил форму регистрации и уставил активность аккаунта по умолчанию False

--- B_Board/personal/forms.py
+++ B_Board/personal/forms.py
@@ -1,3 +1,4 @@
+from allauth.account.forms import SignupForm
 from django import forms
 from ads.models import Response
 
@@ -8,3 +9,11 @@ class ResponseForm(forms.ModelForm):
     class Meta:
         model = Response
         fields = ['accept']
+
+
+class MySignupForm(SignupForm):
+    def save(self, request):
+        user = super(MySignupForm, self).save(request)
+        user.is_active = False
+        user.save()
+        return user
